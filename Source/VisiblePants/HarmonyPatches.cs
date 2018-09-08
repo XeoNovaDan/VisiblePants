9b90946d5e50cfa5f8efd5eb3c7f331445c57544
XeoNovaDan <toghill7252010@hotmail.co.uk>
2018-09-08 18:55:41 +0100

    Now with 10% more C#

---
+++ Source/VisiblePants/HarmonyPatches.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace VisiblePants
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+
+        private static readonly Type patchType = typeof(HarmonyPatches);
+
+        static HarmonyPatches()
+        {
+            HarmonyInstance h = HarmonyInstance.Create("XeoNovaDan.VisiblePants");
+
+            h.Patch(AccessTools.Method(typeof(Pawn_ApparelTracker), "SortWornApparelIntoDrawOrder"),
+                new HarmonyMethod(patchType, nameof(Prefix_SortWornApparelIntoDrawOrder)));
+        }
+
+        public static bool Prefix_SortWornApparelIntoDrawOrder(Pawn_ApparelTracker __instance, ref ThingOwner<Apparel> ___wornApparel)
+        {
+            ___wornApparel.InnerListForReading.Sort((Apparel a, Apparel b) => (a.IsPants() ? 99 : a.def.apparel.LastLayer.drawOrder).CompareTo(b.def.apparel.LastLayer.drawOrder));
+            return false;
+        }
+
+        private static bool IsPants(this Apparel a) => a.def == VP_ThingDefOf.Apparel_Pants || a.def == VP_ThingDefOf.Apparel_FlakPants;
+
+    }
+}
---
+++ Source/VisiblePants/VP_ThingDefOf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VisiblePants
+{
+    [DefOf]
+    public static class VP_ThingDefOf
+    {
+
+        public static ThingDef Apparel_Pants;
+
+        public static ThingDef Apparel_FlakPants;
+
+    }
+}
